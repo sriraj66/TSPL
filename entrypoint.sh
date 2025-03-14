067ec64a2be49a15d9da990e641d740c4bb5998f
Sriram <sriramrajaclg@gmail.com>
2025-03-14 19:14:59 +0530

    fix db

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ chmod +x ./wait_for_db.sh
 ./wait_for_db.sh db:5432 -t 15
 
 python3 manage.py migrate --no-input
+python3 manage.py makemigrations core --no-input
 python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
