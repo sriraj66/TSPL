80b9e3f2f6d254ac08d3d1c40c9513f9f027c1e3
Sriram <sriramrajaclg@gmail.com>
2025-03-17 09:57:00 +0530

    Export Fielsds

--- core/admin.py
+++ core/admin.py
@@ -10,7 +10,7 @@ class PlayerRegistrationResource(resources.ModelResource):
             'id', 'reg_id','user__username', 'player_name', 'father_name',
             'dob', 'gender', 'tshirt_size', 'mobile', 'wathsapp_number', 'email', 'adhar_card',
             'player_image', 'district', 'zone', 'pin_code', 'address', 'first_preference','batting_arm',
-            'role', 'is_paid', 'tx_id', 'created'
+            'role', 'is_paid', 'tx_id', 'is_selected', 'points','created'
         )
         export_order = fields
         
@@ -19,9 +19,9 @@ class PlayerRegistrationResource(resources.ModelResource):
 class PlayerRegistrationAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('reg_id','player_name', 'district', 'zone', 'is_paid', 'created') 
     search_fields = ('reg_id','user__username', 'player_name', 'mobile',
-        'email', 'adhar_card', 'district','tx_id'
+        'email', 'adhar_card', 'district','tx_id', "is_selected", "points"
     )
-    list_filter = ('zone', 'is_paid', 'gender', 'district') 
+    list_filter = ('zone', 'is_paid','is_selected', 'gender', 'district') 
     ordering = ('-created',) 
     readonly_fields = ('reg_id','zone', 'created') 
 
